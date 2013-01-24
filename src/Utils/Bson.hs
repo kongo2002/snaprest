f839fd610a3f0b17e2bcd7883d782ee1f1b4b4ee
kongo2002 <kongo2002@googlemail.com>
2013-01-24 23:35:23 +0100

    add doc to Bson utilities

--- src/Utils/Bson.hs
+++ src/Utils/Bson.hs
@@ -1,6 +1,5 @@
 module Utils.Bson
-    (
-      getList
+    ( getList
     , getMaybe
     , setMaybe
     ) where
@@ -11,13 +10,24 @@ import Data.Maybe     ( mapMaybe )
 
 import Utils.Mongo (MongoType, fromDoc)
 
+
+------------------------------------------------------------------------------
+-- | Helper function to return an empty list intead of Nothing if the
+-- requested field is not found
 getList :: MongoType a => Label -> Document -> Maybe [a]
 getList k doc =
     Just $ maybe [] (mapMaybe fromDoc) (lookup k doc)
 
+
+------------------------------------------------------------------------------
+-- | Helper function to return @Just Nothing@ in case the requested field
+-- is not found
 getMaybe :: Val a => Label -> Document -> Maybe (Maybe a)
 getMaybe k doc = Just $ lookup k doc
 
+
+------------------------------------------------------------------------------
+-- | Helper function to return a @Just@ field based on a given @Maybe@ value
 setMaybe :: Val v => Label -> Maybe v -> Maybe Field
 setMaybe k (Just x) = Just (k =: x)
 setMaybe _ Nothing  = Nothing
